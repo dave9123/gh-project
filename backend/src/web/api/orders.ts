83d3f9cb8518f1b92c026ba7f52e4698c87e8760
Dave <dave.turmawan@outlook.com>
2025-07-24 19:10:15 +0700

    boom

--- backend/src/web/api/orders.ts
+++ backend/src/web/api/orders.ts
@@ -1,14 +1,14 @@
-import express from 'express';
-import db from '../../modules/db';
+import express from "express";
+import db from "../../modules/db";
 const router = express.Router();
 
 
 
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
 
 });
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
 });
 
 export default router;
--- backend/src/web/app.ts
+++ backend/src/web/app.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import orders from './api/orders';
-import enterprise from './api/enterprise';
+import express from "express";
+import orders from "./api/orders";
+import enterprise from "./api/enterprise";
 
 const app = express();
 
-app.use('/api/orders', orders);
+app.use("/api/orders", orders);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
