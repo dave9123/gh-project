a5d40b1fb6f96e90731943d956dd0656c9988eab
Dave <dave.turmawan@outlook.com>
2025-07-24 23:52:33 +0700

    Returning to PostgreSQL

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -5,15 +5,15 @@ export default defineConfig({
     out: './drizzle',
     schema: './src/db/schema.ts',
     dialect: 'postgresql',
-    /*dbCredentials: {
+    dbCredentials: {
         host: process.env.DATABASE_HOST!,
         user: process.env.DATABASE_USER!,
         port: parseInt(process.env.DATABASE_PORT || "5432"),
         password: process.env.DATABASE_PASSWORD!,
         database: process.env.DATABASE_NAME!,
         ssl: "prefer"
-    }*/
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
     }
+    /*dbCredentials: {
+        url: process.env.DATABASE_URL!,
+    }*/
 });
