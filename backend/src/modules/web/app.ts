9bd8a39b8686921ec6ba517171ec59a2406d9dc5
satr14 <90962949+SX-9@users.noreply.github.com>
2025-07-24 16:31:03 +0700

    orders api route

--- backend/src/modules/web/app.ts
+++ backend/src/modules/web/app.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import orders from './api/orders';
 
 const app = express();
+
+app.use('/api/orders', orders);
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
