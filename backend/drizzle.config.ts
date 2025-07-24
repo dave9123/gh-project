a31694426b21bfe6b3462dc8647ad4f528745689
Dave <dave.turmawan@outlook.com>
2025-07-24 22:58:47 +0700

    push to d1

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -4,13 +4,19 @@ import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
     out: './drizzle',
     schema: './src/db/schema.ts',
-    dialect: 'postgresql',
-    dbCredentials: {
+    dialect: 'sqlite',
+    driver: 'd1-http',
+    /*dbCredentials: {
         host: process.env.DATABASE_HOST!,
         user: process.env.DATABASE_USER!,
         port: parseInt(process.env.DATABASE_PORT || "5432"),
         password: process.env.DATABASE_PASSWORD!,
         database: process.env.DATABASE_NAME!,
         ssl: "prefer"
+    }*/
+    dbCredentials: {
+        accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+        databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+        token: process.env.CLOUDFLARE_D1_TOKEN!,
     }
 });
