4d1880d071a56a1803be13a392f042803d471284
Dave <dave.turmawan@outlook.com>
2025-07-24 16:47:03 +0700

    db

---
+++ backend/src/types/index.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            DATABASE_HOST: string;
+            DATABASE_USER: string;
+            DATABASE_PASSWORD: string;
+            DATABASE_NAME: string;
+            DATABASE_PORT?: string;
+            DATABASE_SSL?: string;
+            DATABASE_REJECT_UNAUTHORIZED?: string;
+        }
+    }
+}
