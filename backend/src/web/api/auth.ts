9fba4e84b115920a3877aebee3ed61dfa16c2f76
satr14 <90962949+SX-9@users.noreply.github.com>
2025-07-25 01:33:19 +0700

    jwt

--- backend/src/web/api/auth.ts
+++ backend/src/web/api/auth.ts
@@ -1,7 +1,15 @@
 import express from "express";
 import db from "../../modules/db";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
-router.post("/oauth", (req, res) => {});
+router.post("/oauth", (req, res) => {
+  const token = jwt.sign({
+    email: req.body.email
+  }, process.env.JWT_SECRET!, {
+    expiresIn: "1h"
+  });
+  res.json({ token });
+});
 
 export default router;
--- backend/src/web/app.ts
+++ backend/src/web/app.ts
@@ -1,4 +1,13 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
 
 import auth from "./api/auth";
 import enterprise from "./api/enterprise";
@@ -8,6 +17,19 @@ import orders from "./api/orders";
 const app = express();
 
 app.use(express.json());
+app.use((req, res, next) => {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
+    console.error(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  })
+});
 
 app.use("/api/orders", orders);
 app.use("/api/auth", auth);
