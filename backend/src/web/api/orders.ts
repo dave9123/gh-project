c164c5552201415308b4176769b824cdfa3c9a59
satr14 <90962949+SX-9@users.noreply.github.com>
2025-07-25 08:55:42 +0700

    orders implementation draft

--- backend/src/web/api/orders.ts
+++ backend/src/web/api/orders.ts
@@ -1,8 +1,28 @@
 import express from "express";
 import db from "../../modules/db";
+import { ordersTable } from "../../db/schema";
 const router = express.Router();
 
-router.post("/", (req, res) => {});
-router.get("/", (req, res) => {});
+router.post("/", (req, res) => {
+  try {
+    const { id, fieldId, formValues, currency, total, status } = req.body;
+
+    db.insert(ordersTable).values({
+      
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.get("/", (req, res) => {
+  try {
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 export default router;
