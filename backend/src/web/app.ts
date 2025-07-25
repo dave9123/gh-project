3b62cd98e4aa2234e62cd11290bc4dded434e80e
Dave <dave.turmawan@outlook.com>
2025-07-25 14:21:15 +0700

    Bypass authorization for AI

--- backend/src/web/app.ts
+++ backend/src/web/app.ts
@@ -8,45 +8,46 @@ import express from "express";
 import jwt from "jsonwebtoken";
 
 declare global {
-  namespace Express {
-    interface Request {
-      user?: any;
+    namespace Express {
+        interface Request {
+            user?: any;
+        }
     }
-  }
 }
 
 const app = express();
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(204);
+    }
+    next();
 });
 
 app.use(express.json());
 app.get("/ping", (_, res) => res.status(200).send("Pong!"));
 app.use((req, res, next) => {
-  if (req.path.startsWith("/api/auth")) return next();
+    if (req.path.startsWith("/api/auth")) return next();
+    if (req.path.startsWith("/api/ai")) return next();
 
-  console.log("Request received:", req.method, req.path, req.headers);
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+    console.log("Request received:", req.method, req.path, req.headers);
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) return res.sendStatus(401);
+    if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
-    if (err) {
-      console.error("An error occured while verifying JWT", err);
-      return res.sendStatus(500).json({ error: "Internal Server Error" });
-    }
-    console.log("User authenticated:", user);
-    req.user = user;
-    next();
-  });
+    jwt.verify(token, process.env.JWT_SECRET as string, (err: any, user: any) => {
+        if (err) {
+            console.error("An error occured while verifying JWT", err);
+            return res.sendStatus(500).json({ error: "Internal Server Error" });
+        }
+        console.log("User authenticated:", user);
+        req.user = user;
+        next();
+    });
 });
 
 app.use("/api/ai", ai);
@@ -56,7 +57,7 @@ app.use("/api/business", business);
 app.use("/api/files", files);
 
 export default function startServer() {
-  const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 3000;
 
-  app.listen(port, () => console.log(`Server is running on port ${port}`));
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
 } 
