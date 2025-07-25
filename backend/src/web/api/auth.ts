4cfb50361b498198fadb8a4c28008d4e63a75f27
satr14 <90962949+SX-9@users.noreply.github.com>
2025-07-25 16:19:38 +0700

    fix auth possible duplicate email

--- backend/src/web/api/auth.ts
+++ backend/src/web/api/auth.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "../../modules/db";
 import jwt from "jsonwebtoken";
+import { eq } from "drizzle-orm";
 
 import { usersTable } from "../../db/schema";
 
@@ -8,14 +9,16 @@ const router = express.Router();
 
 router.post("/oauth", async (req, res) => {
   try {
+    // check if the user already exists
     const token = jwt.sign(
       {
         email: req.body.email,
       },
       process.env.JWT_SECRET!
     );
-
-    await db.insert(usersTable).values({
+    
+    const existingUser = await db.select().from(usersTable).where(eq(usersTable.email, req.body.email));
+    if (existingUser.length == 0) await db.insert(usersTable).values({
       email: req.body.email,
       provider: req.body.provider,
     });
@@ -28,3 +31,4 @@ router.post("/oauth", async (req, res) => {
 });
 
 export default router;
+
