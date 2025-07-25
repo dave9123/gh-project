5765378149b42b011902dce93930779cf2020b78
Aldhanekaa <aldhanekadev@gmail.com>
2025-07-25 08:44:09 +0700

    test

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,70 +1,45 @@
-import { desc, relations } from "drizzle-orm";
-import { integer, pgTable, varchar, timestamp, boolean } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  integer,
+  pgTable,
+  varchar,
+  timestamp,
+  boolean,
+} from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
 
-    email: varchar().notNull().unique(),
-    provider: varchar().notNull(),
+  email: varchar().notNull().unique(),
+  provider: varchar().notNull(),
 
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull(),
-    lastUsed: timestamp().defaultNow().notNull()
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
+  lastUsed: timestamp().defaultNow().notNull(),
 });
 
 export const productsTable = pgTable("products", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-    
-    name: varchar().notNull(),
-    description: varchar().default("").notNull(),
-    basePrice: integer().notNull(),
-    currencyType: varchar().default("IDR").notNull(),
-    business: integer().notNull().references(() => businessTable.id),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
 
-    createdAt: timestamp().defaultNow().notNull(),
-    lastModified: timestamp().defaultNow().notNull()
+  name: varchar().notNull(),
+  description: varchar().default("").notNull(),
+  basePrice: integer().notNull(),
+  currencyType: varchar().default("IDR").notNull(),
+
+  createdAt: timestamp().defaultNow().notNull(),
+  lastModified: timestamp().defaultNow().notNull(),
 });
 
 export const businessTable = pgTable("business", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
 
-    name: varchar().notNull(),
-    slug: varchar().notNull(),
-    phoneNumber: varchar().notNull(),
-    ownerEmail: varchar().notNull().references(() => usersTable.email),
+  name: varchar().notNull(),
+  slug: varchar().notNull(),
+  phoneNumber: varchar().notNull(),
+  ownerEmail: varchar()
+    .notNull()
+    .references(() => usersTable.email),
 
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull()
-});
-
-export const ordersTable = pgTable("orders", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-
-    name: varchar().notNull(),
-    description: varchar().default("").notNull(),
-    currency: varchar().default("IDR").notNull(),
-    status: varchar().default("pending").notNull(),
-
-    userId: integer().notNull().references(() => usersTable.id),
-    businessId: integer().notNull().references(() => businessTable.id),
-    productId: integer().notNull().references(() => productsTable.id),
-    fileId: integer().notNull(),
-
-    quantity: integer().notNull(),
-    totalPrice: integer().notNull(),
-
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull()
-});
-
-export const filesTable = pgTable("files", {
-    id: integer().primaryKey().generatedAlwaysAsIdentity(),
-
-    filename: varchar().notNull(),
-    originalname: varchar().notNull(),
-    mimetype: varchar().notNull(),
-    size: integer().notNull(),
-
-    createdAt: timestamp().defaultNow().notNull(),
-    updatedAt: timestamp().defaultNow().notNull()
+  createdAt: timestamp().defaultNow().notNull(),
+  updatedAt: timestamp().defaultNow().notNull(),
 });
