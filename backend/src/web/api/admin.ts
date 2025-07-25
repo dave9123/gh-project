7b29ed931a77baff883f4391ff16eb2d5a27f07e
satr14 <90962949+SX-9@users.noreply.github.com>
2025-07-25 17:14:35 +0700

    implement admin route

--- backend/src/web/api/admin.ts
+++ backend/src/web/api/admin.ts
@@ -1,16 +1,31 @@
 import express from "express";
 import db from "../../modules/db";
+import { businessTable, ordersTable } from "../../db/schema";
+import { eq } from "drizzle-orm";
 const router = express.Router();
 
-router.get("/orders", (req, res) => {
-
+router.get("/orders", async (req, res) => {
+  try {
+    const businessResult = await db.select().from(businessTable).where(eq(businessTable.ownerEmail, req.user.email));
+    const businessId = businessResult[0]?.id;
+    if (!businessId) return res.status(404).json({ error: "User does not own a business" });
+    const orders = await db.select().from(ordersTable).where(eq(ordersTable.businessId, businessId));
+    
+    res.status(200).json(orders);
+  } catch (error) {
+    console.error("Error in /orders route:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
-router.put("/order/:id", (req, res) => {
+router.put("/order/:orderId", async (req, res) => {
   try {
-    const { id } = req.params;
+    const { orderId } = req.params;
+    const { newStatus } = req.body;
 
-    res.send(req.user);
+    await db.update(ordersTable).set({ status: newStatus }).where(eq(ordersTable.id, parseInt(orderId)));
+
+    res.status(200).json({ message: "Order updated successfully" });
   } catch (error) {
     console.error("Error updating order:", error);
     res.status(500).json({ error: "Internal Server Error" });
